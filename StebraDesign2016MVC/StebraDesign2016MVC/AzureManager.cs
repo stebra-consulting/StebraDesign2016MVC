370b941029999479c47a6a5a27b57e2df2a65d3a
felix <felix@stebra.se>
2016-01-19 17:42:29 +0100

    added AzureManager

---
+++ StebraDesign2016MVC/StebraDesign2016MVC/AzureManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using StebraDesign2016MVC.Models;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace StebraDesign2016MVC
+{
+    public class AzureManager
+    {
+        //Config
+        private const string tableName = "stebraNyhetslist";
+        private const string partitionKey = "Nyhet";
+
+        //Connection to Azure Storage
+        private static CloudStorageAccount StorageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+        //get Table -client  
+        public static CloudTableClient tableClient = StorageAccount.CreateCloudTableClient();
+
+        public static List<StebraEntity> LoadNews()
+        {
+
+            CloudTable table = SelectValidTable();
+
+            //Query all entities where "PartitionKey" is "News"
+            var allNewsQuery = new TableQuery<StebraEntity>()
+                .Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, partitionKey));//partitionKey is "Nyhet"
+
+            //Entities to List
+            var news = table.ExecuteQuery(allNewsQuery).ToList();
+
+            if (LinkManager.globalNews == null)
+            {
+                LinkManager.globalNews = news;
+
+            }
+
+            //Return List
+            return news;
+
+
+        }
+
+
+        public static CloudTable SelectValidTable()
+        {
+            CloudTable tempTable = null;
+
+            for (int id = 0; id < 10; id++)
+            {
+                tempTable = tableClient.GetTableReference(tableName + id.ToString());
+                if (tempTable.Exists()) break;
+            }
+            //check this tables
+
+            return tempTable;
+        }
+    }
+
+}
---
+++ StebraDesign2016MVC/StebraDesign2016MVC/Scripts/Design.js
@@ -0,0 +1,66 @@
+﻿'use strict';
+
+$(document).ready(function () {
+    $('.carousel').carousel();
+
+
+    var heights = $(".mydiv").map(function () {
+        return $(this).height();
+    }).get(),
+
+    maxHeight = Math.max.apply(null, heights);
+
+    $(".mydiv").height(maxHeight);
+
+    ResponsiveResize();
+});
+
+function ResponsiveResize() {
+    $(window).resize(function () {
+        var heights = $(".mydiv").map(function () {
+            return $(this).height();
+        }).get(),
+
+    maxHeight = Math.max.apply(null, heights);
+
+        $(".mydiv").height(maxHeight);
+    });
+}
+
+//------simons js kod------------------------------------
+//-------------------------------------------------------
+//$(document).ready(function () {
+    
+//    var heights = $(".mydiv").map(function () {
+//        return $(this).height();
+//    }).get();
+//​
+//    var normalHeight = heights;
+//​
+//    maxHeight = Math.max.apply(null, heights);
+//​
+//    $(".mydiv").height(maxHeight);
+//​
+//    $(window).resize(function () {
+//   ​
+//        ResponsiveResize(normalHeight);
+//    });
+//});
+//​
+//function ResponsiveResize(normalHeight) {
+   
+//    var heights;
+//    $(".mydiv").html(" ");
+//​
+//​
+//​
+//    heights = window.innerHeight;
+//​
+//​
+//​
+//    $(".mydiv").append(heights);
+//​
+//    $(".mydiv").height(heights/10);
+    
+//}
+//----------------------------------------------------
