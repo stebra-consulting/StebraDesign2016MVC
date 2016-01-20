9d1b063ab1b47527ef6471b1cbaa6c5726bd1da3
felix <felix@stebra.se>
2016-01-20 10:53:23 +0100

    Added Responsive class to all <img>

---
+++ StebraDesign2016MVC/StebraDesign2016MVC/Scripts/App.js
@@ -0,0 +1,13 @@
+﻿'use strict';
+
+
+//  TEMPORÄR LÖSNING FÖR ATT FÖRMINSKA ALLA IMG TAGGAR I item.Body  //
+$(document).ready(
+    function () {
+        $('img').each(
+            function () {
+                $(this).addClass('thumbnail img-responsive');
+            }
+        );
+    }
+);
